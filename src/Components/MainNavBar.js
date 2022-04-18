f74967ee5839c6261db4adae722e2ce53fe346f1
kale <kalerampenthal@gmail.com>
2022-04-18 15:22:22 -0600

    added navbar and basic form

--- src/Components/MainNavBar.js
+++ src/Components/MainNavBar.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import { NavLink } from 'react-router-dom'
 
 const MainNavBar = () => {
   return (
-    <div>MainNavBar</div>
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+        <Navbar.Brand to="/">iCrush</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav.Link><NavLink to='/'>Home</NavLink></Nav.Link>
+          <Nav className="me-auto">
+            <NavDropdown title="Goals" id="collasible-nav-dropdown">
+              <NavDropdown.Item><NavLink to='/dashboard'>All</NavLink></NavDropdown.Item>
+              <NavDropdown.Item><NavLink to='/dashboard-daily'>Daily</NavLink></NavDropdown.Item>
+              <NavDropdown.Item><NavLink to='/dashboard-weekly'>Weekly</NavLink></NavDropdown.Item>
+              <NavDropdown.Item><NavLink to='/dashboard-monthly'>Monthly</NavLink></NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link><NavLink to='/stats'>Stats</NavLink></Nav.Link>
+            <Nav.Link><NavLink to='/create-new-task'>New Task</NavLink></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
--- src/Pages/Form/CreateTaskForm.js
+++ src/Pages/Form/CreateTaskForm.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import './CreateTaskForm.css'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const CreateTaskForm = () => {
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+  
   return (
-    <div>CreateTaskForm</div>
+    <div className="task-form">
+      <h2>Create New Task</h2>
+      <Form onClick={handleSubmit}>
+        
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Task Title</Form.Label>
+          <Form.Control type="email" placeholder="Enter task" />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Task Description</Form.Label>
+          <Form.Control type="text" as="textarea" placeholder="Description" />
+        </Form.Group>
+       
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Complete By</Form.Label>
+          <Form.Control type="date" />
+        </Form.Group>
+        
+        <Form.Label>Category</Form.Label>
+        <Form.Select aria-label="Default select example">
+          <option>Select Category</option>
+          <option value="1">Home</option>
+          <option value="2">Work</option>
+          <option value="3">School</option>
+        </Form.Select>
+
+        
+
+
+        <Button variant="primary" type="submit">
+          Add Task
+        </Button>
+      </Form>
+    </div>
   )
 }
 
